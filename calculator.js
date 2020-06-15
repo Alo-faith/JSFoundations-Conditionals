4103dcdd448dfd63b7dd4504f4133027d8bb9b8e
alo-faith <alo.faith@gmail.com>
2020-06-15 19:53:11 +0300

    new calculator

--- calculator.js
+++ calculator.js
@@ -1,3 +1,16 @@
 // Define the numbers and the operation here
-
+let first = 50;
+let second = 10;
+let operation = "+";
 // Write if-statements here
+if (operation === "/") {
+  console.log(first / second);
+} else if (operation === "+") {
+  console.log(first + second);
+} else if (operation === "-") {
+  console.log(first - second);
+} else if (operation === "*") {
+  console.log(first * second);
+} else {
+  console.log("Error input");
+}
